5596447eb5a1667aa15a196b1553d63124b7e2da
徐登伟 <1103579820@qq.com>
2018-04-19 14:47:44 +0800

    modify content of application

--- publicLayout/main.js
+++ publicLayout/main.js
@@ -5,9 +5,11 @@
  * @desc [description]
 */
 const header = require('../templateSource/commonhtml/header.html');
+const nav = require('../templateSource/commonhtml/nav.html');
 const topbar = require('../templateSource/commonhtml/topbar.html');
 const footer = require('../templateSource/commonhtml/footer.html');
 const htmlconfig = {
+    nav: nav,
     header: header,
     topbar: topbar,
     footer: footer
--- templateSource/course/course.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author [xudengwei.com]
- * @email [1103579820@qq.com]
- * @date 2017-02-24 02:51:34
- * @desc [description]
-*/
-const publiclayout = require('../../publicLayout/main.js');
-const main_course = require('./course.ejs');
-module.exports = main_course(publiclayout);
